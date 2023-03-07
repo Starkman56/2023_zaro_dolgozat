ef4355036d04e33abb4c5b39a72f58655ffd2255
Starkman56 <75947368+Starkman56@users.noreply.github.com>
2023-03-07 09:13:26 +0100

    szabo

--- Projektmunka/components/footer.php
+++ Projektmunka/components/footer.php
@@ -2,9 +2,11 @@
        <div class="row justify-content-center mb-0 pt-5 pb-0 row-2 px-3">
            <div class="col-12">
                 <div class="row row-2 ">
-                   
-                    <div class="col-sm-4  my-sm-0 mt-5"><ul class="list-unstyled"><li class="">Platform</li><li>Help Center</li><li>Security</li></ul></div>
-                    <div class="col-sm-4  my-sm-0 mt-5"><ul class="list-unstyled"><li class="">Customers</li><li>Use Cases</li><li>Customers Services</li></ul>
+                    
+                    <div class="col-sm-4  my-sm-0 mt-5"><ul class="list-unstyled"><li class="">Kordics Zoltán  - egyéni vállalkozó</li><li>Telephely : Balassagyarmat Rákóczi út 72-74</li><li>Székhely : Balassagyarmat Rákoczi út 72-74</li><li>Adószám : 49268715-2-32 </li><li>Nyilvántartási szám : 461</li><li>Nyilvántartásba vevő szerv : Balassagyarmat város Jegyzője</li></ul></div>
+                    <div class="col-sm-4  my-sm-0 mt-5"><ul class="list-unstyled"><li class=""></li><li>Bankszámlaszám : MKB Bank 10300002-10642542-49030017</li><li>Cím : 2660 Balassagyarmat, Rákóczi út 72-74.</li><li>Tel	: 06-30/698-93-91, 06-30/27 88 073</li><li>Nyitvatartás	:	Hétfő-péntek: 09.00 - 17.00
+                    <li>Szombat: 08.00 - 13.00</li>
+                    Vasárnap: zárva</li></ul>
                     </div>
                     <div class="col-sm-4  my-sm-0 mt-5"><ul class="list-unstyled"><li class=""><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2665.54694621034!2d19.29940541588336!3d48.08038726312102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476aa079e388b043%3A0xd91a09ed7e7857af!2sBalassagyarmat%2C%20R%C3%A1k%C3%B3czi%20fejedelem%20%C3%BAt%2072%2C%202660!5e0!3m2!1shu!2shu!4v1678103399399!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li></li></ul>
                   </div>                   
@@ -14,8 +16,10 @@
        <div class="row justify-content-center mt-0 pt-0 row-1 mb-0  px-sm-3 px-2">
             <div class="col-12">
                 <div class="row my-4 row-1 no-gutters">
-                    <div class="col-sm-3 col-auto text-center"><small>&#9400; Kordics Balázs & Szabó Máté</small></div><div class="col-md-3 col-auto "></div><div class="col-md-3 col-auto"></div>
-                    <div class="col  my-auto text-md-left  text-right "> <small> hello@getstride.com <span><img src="https://i.imgur.com/TtB6MDc.png" class="img-fluid "  width="25"></span> <span><img src="https://i.imgur.com/N90KDYM.png" class="img-fluid "  width="25"></span></small>  </div> 
+                    <div class="col-sm-3 col-auto text-center"><small>&#9400; Kordics Balázs & Szabó Máté</small></div>
+                    <div class="col-md-3 col-auto ">Horgász - Díszállat - Háztartási bolt</div>
+                    <div class="col-md-3 col-auto"></div>
+                    <div class="col  my-auto text-md-left  text-right "> <small> hello@gmail.com <span><img src="https://i.imgur.com/TtB6MDc.png" class="img-fluid "  width="25"></span> <span><img src="https://i.imgur.com/N90KDYM.png" class="img-fluid "  width="25"></span></small>  </div> 
                 </div>
             </div>
         </div>
