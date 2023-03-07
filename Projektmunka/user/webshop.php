48acb7f435cee5be974f0de2e803ca3382700fc2
Starkman56 <75947368+Starkman56@users.noreply.github.com>
2023-03-07 08:01:15 +0100

    Szabo

--- Projektmunka/user/webshop.php
+++ Projektmunka/user/webshop.php
@@ -98,6 +98,7 @@ require("../components/nav.php");
   <img class="webshopkep" src="../keps/madareteto.jpg" alt=""><figcaption>Fatermékek</figcaption>
   </div>
 </div>
+
     <!-- nav scriptek -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
@@ -109,6 +110,30 @@ require("../components/nav.php");
 
     <!-- nav scriptek -->
     <script src="../js/script.js"></script>
-</body>
+    <div class="container-fluid pb-0 mb-0 justify-content-center text-light ">
+    <footer >
+       <div class="row justify-content-center mb-0 pt-5 pb-0 row-2 px-3">
+           <div class="col-12">
+                <div class="row row-2">
+                   
+                    <div class="col-sm-3  my-sm-0 mt-5"><ul class="list-unstyled"><li class="mt-0">Platform</li><li>Help Center</li><li>Security</li></ul></div>
+                    <div class="col-sm-3  my-sm-0 mt-5"><ul class="list-unstyled"><li class="mt-0">Customers</li><li>Use Cases</li><li>Customers Services</li></ul>
+                    </div>
+                    <div class="col-sm-3  my-sm-0 mt-5"><ul class="list-unstyled"><li class="mt-0"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2665.54694621034!2d19.29940541588336!3d48.08038726312102!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476aa079e388b043%3A0xd91a09ed7e7857af!2sBalassagyarmat%2C%20R%C3%A1k%C3%B3czi%20fejedelem%20%C3%BAt%2072%2C%202660!5e0!3m2!1shu!2shu!4v1678103399399!5m2!1shu!2shu" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li></li></ul>
+                  </div>                   
+                </div>  
+           </div>
+       </div>
+       <div class="row justify-content-center mt-0 pt-0 row-1 mb-0  px-sm-3 px-2">
+            <div class="col-12">
+                <div class="row my-4 row-1 no-gutters">
+                    <div class="col-sm-3 col-auto text-center"><small>&#9400; Kordics Balázs & Szabó Máté</small></div><div class="col-md-3 col-auto "></div><div class="col-md-3 col-auto"></div>
+                    <div class="col  my-auto text-md-left  text-right "> <small> hello@getstride.com <span><img src="https://i.imgur.com/TtB6MDc.png" class="img-fluid "  width="25"></span> <span><img src="https://i.imgur.com/N90KDYM.png" class="img-fluid "  width="25"></span></small>  </div> 
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+  </body>
 
 </html>
