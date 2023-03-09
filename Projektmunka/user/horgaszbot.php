0f289eb230a57bcd8d0efc00a9a8fd6b51b1f6eb
Starkman56 <75947368+Starkman56@users.noreply.github.com>
2023-03-09 07:58:46 +0100

    szabo

--- Projektmunka/user/horgaszbot.php
+++ Projektmunka/user/horgaszbot.php
@@ -18,15 +18,11 @@ INNER JOIN kategoriak
 ON kategoria_id = termek.alkategoria_id
 WHERE alkategoria_id = 1";
 require("../components/beolvas.php");
-
-
 ?>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&amp;display=swap" rel="stylesheet">
@@ -42,14 +38,6 @@ require("../components/beolvas.php");
     <!--nav  linkek -->
 </head>
 <body>
-
-
-
-
-
-
-
-
     <!--Website container-->
     <div class="container">
         <div class="cards">
@@ -58,9 +46,6 @@ require("../components/beolvas.php");
             ?>
         </div>
     </div>
-
-    
-
     <!-- nav scriptek -->
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/popper.min.js"></script>
@@ -69,7 +54,6 @@ require("../components/beolvas.php");
     <script defer="" src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon="{&quot;rayId&quot;:&quot;799ed0aa7ac3c24a&quot;,&quot;token&quot;:&quot;cd0b4b3a733644fc843ef0b185f98241&quot;,&quot;version&quot;:&quot;2023.2.0&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="navbar.js"></script>
-
     <!-- nav scriptek -->
     <script src="../js/script.js"></script>
 </body>
