44eb05851994f13b8295d890fe24530b455c113b
Kordics <112695690+Kordics@users.noreply.github.com>
2023-03-20 12:01:31 +0100

    vissza

--- Projektmunka/register.php
+++ Projektmunka/register.php
@@ -1,57 +1,60 @@
 <?php
 
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //lapvédelem
 session_start();
 
+
 require("kapcsolat/kapcs.php");
 if(isset($_POST['ok']))
 {
-    $nev = mysqli_real_escape_string($dbconn, $_POST['nev']);
-    $felhnev = mysqli_real_escape_string($dbconn, $_POST['felhnev']);
+    $name = mysqli_real_escape_string($dbconn, $_POST['name']);
+    $username = mysqli_real_escape_string($dbconn, $_POST['username']);
     $email = mysqli_real_escape_string($dbconn, $_POST['email']);
-    $jelszo = sha1($_POST['jelszo']);
-    $jelszoujra = sha1($_POST['jelszoujra']);
+    $pass = sha1($_POST['password']);
+    $passagain = sha1($_POST['passwordagain']);
     $usertype = $_POST['user_type'];
-    $iranyitoszam = strip_tags($dbconn, $_POST['iranyitoszam']);
-    $telepules = strip_tags($dbconn, $_POST['telepules']);
-    $szallitasicim = strip_tags($dbconn, $_POST['szallitasicim']);
-    
 
-    $select = "SELECT * FROM `szemelyek`
-               WHERE email = '$email' && jelszo = '$jelszo'";
+
+    $select = "SELECT * FROM `accounts`
+               WHERE email = '$email' && password = '$pass'";
+
 
     $result = mysqli_query($dbconn, $select);
 
+
     if(mysqli_num_rows($result) > 0)
     {
-        $error[] = 'Felhasználó már léttezik!';
+        $error[] = 'user already exist!';
     }
     else
     {
-        if($jelszo != $jelszoujra)
+        if($pass != $passagain)
         {
-            $error[] = 'Jelszó nem egyezik!';
+            $error[] = 'passord not matched!';
         }
         else
         {
-            $insert = "INSERT INTO `accounts`(`nev`, `felhnev`, `email`, `jelszo`, `permission` , `iranyitoszam` , `telepules` , `szallitasicim`) VALUES ('$nev','$felhnev','$email','$jelszo','$usertype' ,'$iranyitoszam' ,'$telepules' ,'$szallitasicim')";
+            $insert = "INSERT INTO `accounts`(`name`, `username`, `email`, `password`, `permission`) VALUES ('$name','$username','$email','$pass','$usertype')";
             mysqli_query($dbconn,$insert);
             header('Location:belep.php');
         }
     }
 }
 
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta nev="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/registerbele2.css">
 </head>
@@ -73,52 +76,52 @@ if(isset($_POST['ok']))
         </div>
         <form id="fom" method="post">
         <p>
-                <div class="melle"><label for="nev">
-                    <p>nev</p>
-                    <input type="text" nev="nev" id="nev" required>
+                <div class="melle"><label for="name">
+                    <p>Name</p>
+                    <input type="text" name="name" id="name" required>
                 </label></div>
-                
+               
             </p>
         <p>
-        <div class="melle"><label for="felhnev">
-                    <p>felhnev</p>
-                    <input type="text" nev="felhnev" id="felhnev" required>
-                </label></div>
-                
-            </p>
-            <p>
-            <div class="melle"> <label for="email">
-                    <p>E-mail</p>
-                    <input type="email" nev="email" id="email" required>
+        <div class="melle"><label for="username">
+                    <p>Username</p>
+                    <input type="text" name="username" id="username" required>
                 </label></div>
                
             </p>
             <p>
-            <div class="melle"><label for="jelszo">
-                    <p>Jelszó</p>
-                    <input type="jelszo" nev="jelszo" id="jelszo" required>
+            <div class="melle"> <label for="email">
+                    <p>E-mail</p>
+                    <input type="email" name="email" id="email" required>
                 </label></div>
-                
+               
             </p>
             <p>
-            <div class="melle"><label for="jelszoujra">
-                    <p>Jelszó újra</p>
-                    <input type="jelszo" nev="jelszoujra" id="jelszoujra" required>
+            <div class="melle"><label for="password">
+                    <p>Jelszó</p>
+                    <input type="password" name="password" id="password" required>
                 </label></div>
-                
+               
+            </p>
+            <p>
+            <div class="melle"><label for="passwordagain">
+                    <p>Jelszó újra</p>
+                    <input type="password" name="passwordagain" id="passwordagain" required>
+                </label></div>
+               
             </p>
             <div class="melles">
-                <select nev="user_type">
+                <select name="user_type">
                     <option value="user">User</option>
                     <option value="admin">Admin</option>
                     </select>
-                
+               
                 </div>
-            <div class="gomb"><input type="submit" value="OK" id="ok" nev="ok"></div>
-            
+            <div class="gomb"><input type="submit" value="OK" id="ok" name="ok"></div>
+           
         </form>
         </div>
-        
+       
     </div>
     <div class="kozep2">
     <p class="nincs">Van már fiókod? <a href="belep.php">Igen</a></p>
@@ -131,5 +134,13 @@ if(isset($_POST['ok']))
     <script src="js/hater.js"></script>
 </body>
 </html>
+
+
+
+
+
+
+
+
 
 
