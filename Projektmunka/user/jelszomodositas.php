a95dd68722d8bd3f3aca5932f4555ce4ace1ef37
Kordics <112695690+Kordics@users.noreply.github.com>
2023-04-19 08:54:52 +0200

    jelszomodositas

--- Projektmunka/user/jelszomodositas.php
+++ Projektmunka/user/jelszomodositas.php
@@ -1,5 +1,3 @@
-
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -13,37 +11,32 @@ require("../kapcsolat/kapcs.php");
 $sql = "SELECT * from szemelyek WHERE id = {$_SESSION['id']}";
 $eredmeny = mysqli_query($dbconn, $sql);
 
-
-
-
 if (isset($_POST['ok'])) {
     $jelszo = sha1($_POST['jelszo']);
     $jelszoujra = sha1($_POST['jelszoujra']);
-   
+    $regijelszo = sha1($_POST['regijelszo']);
+
+    // Check if old password matches
+    $sor = mysqli_fetch_assoc($eredmeny);
+    $oldjelszo = $sor['jelszo'];
 
 
-    if($jelszo != $jelszoujra)
-        {
-            $error[] = 'A jelszavak nem egyeznek!';
-        }
-        else
-        {
-            $sql = "UPDATE szemelyek SET jelszo = '{$jelszo}'";
-                mysqli_query($dbconn, $sql);
-                header('Location:webshop.php');
-        }
-       
+    if ($oldjelszo !== $regijelszo) {
+        $error[] = 'Nem egyezik a régi jelszó!';
+    } else if ($jelszo !== $jelszoujra) {
+        $error[] = 'Nem egyezik a 2 új jelszó';
+    } else {
+        $sql = "UPDATE szemelyek SET jelszo = '{$jelszo}'";
+        mysqli_query($dbconn, $sql);
+        header('Location:webshop.php');
+    }
+
+
 } else {
-
-
     $sor = mysqli_fetch_assoc($eredmeny);
     $jelszo = $sor['jelszo'];
-   
 }
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -56,9 +49,7 @@ if (isset($_POST['ok'])) {
 </head>
 <body>
     <div class="background" id="background"></div>
-   
-       
-        <form method="post">
+    <form method="post">
         <?php
         if(isset($error))
         {
@@ -68,38 +59,18 @@ if (isset($_POST['ok'])) {
             }
         }
         ?>
-
-
-            <p class="jel">Jelszó módosítása</p>
-           <!--  jelszo -->
-           <div class="bevitel">
-                <input type="password" name="jelszo" id="jelszo"  placeholder="Jelszó"required>
-            </div>
+        <p class="jel">Jelszó módosítása</p>
+        <!-- regijelszo -->
+        <div class="bevitel">
+            <input type="password" name="regijelszo" id="regijelszo"  placeholder="Régi jelszó" required>
+        </div><br>
+        <!-- jelszo -->
+        <div class="bevitel">
+            <input type="password" name="jelszo" id="jelszo"  placeholder="Új jelszó" required>
+        </div>
         <!-- jelszoujra -->
-            <div class="bevitel jelszoujra">
-                <input type="password" name="jelszoujra" id="jelszoujra" placeholder="Jelszó újra" required>
-            </div>
-            <div class="gomb"><input type="submit" value="Mentés" id="ok" name="ok"></div>
-            <a href="main.php">Vissza!</a>
-
-
-    </form>
-     
-   
-           
- 
-   
-    <script src="js/hater.js"></script>
-   
-</body>
-
-
-</html>
-
-
-
-
-
-
-
-
+        <div class="bevitel jelszoujra">
+            <input type="password" name="jelszoujra" id="jelszoujra" placeholder="Új jelszó újra" required>
+        </div>
+        <div class="gomb"><input type="submit" value="Mentés" id="ok" name="ok"></div>
+        <a href="main.php">Vissza!</a>
